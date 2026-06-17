ce5d4d69986691ae935eac320d431f25edc3c39b
Mavula06 <mahlalela06@yahoo.com>
2026-06-17 23:52:07 +0200

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,13 @@
+// app/(auth)/_layout.tsx
+import { Stack } from 'expo-router';
+
+export default function AuthLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false, animation: 'fade' }}>
+      <Stack.Screen name="welcome" />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+      <Stack.Screen name="forgot-password" />
+    </Stack>
+  );
+}
